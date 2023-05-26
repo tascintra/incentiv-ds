2f7ff9acbc05b5184d406535b5adf012a7117336
Tiago Cintra <tiago.slv@hotmail.com>
2023-05-26 09:49:50 -0300

    feat(component): head molecule menu component created

---
+++ src/components/molecules/Head.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import clsx from 'clsx'
+import MenuIcon from '@/assets/icon/menu-icon.svg'
+import IncentivLogo from '@/assets/logo/incentiv-logo.svg'
+
+interface MenuElement extends React.MenuHTMLAttributes<HTMLElement> {}
+
+export default function Head({ className, ...props }: MenuElement) {
+  return (
+    <menu className={clsx('flex w-full gap-3', className)} {...props}>
+      <li>
+        <button>
+          <Image src={MenuIcon} alt="Ícone Menu" />
+        </button>
+      </li>
+      <li>
+        <a href="/">
+          <Image src={IncentivLogo} alt="Logo Incentiv" />
+        </a>
+      </li>
+    </menu>
+  )
+}
