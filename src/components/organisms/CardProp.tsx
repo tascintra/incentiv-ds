eccf8db0875871229db6dd726c3ce3b5a3d98aa5
Tiago Cintra <tiago.slv@hotmail.com>
2023-05-29 00:01:44 -0300

    refactor(organism card): component structure done

--- src/components/organisms/CardProp.tsx
+++ src/components/organisms/CardProp.tsx
@@ -1,19 +1,50 @@
 import Image from 'next/image'
-import NubankAvatar from '@/assets/logo/nubank.svg'
-import Text from '../atoms/Text'
+import clsx from 'clsx'
+import Text from '@/components/atoms/Text'
+import Tag from '@/components/organisms/Tag'
 
-interface CardProps {}
+interface CardProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  companyAvatar: HTMLImageElement
+  companyName: string
+  projectName: string
+  tagsInfo: {
+    time: string
+    rating: string
+    icon: 'heart'
+  }
+}
 
-export default function CardProp({}: CardProps) {
+export default function CardProp({
+  className,
+  companyAvatar,
+  companyName,
+  projectName,
+  tagsInfo,
+  ...props
+}: CardProps) {
   return (
-    <div className="max-w-[240px] rounded-lg border bg-white p-4 shadow-xs">
-      <Image src={NubankAvatar} alt="Avatar Nubank" />
+    <div
+      className={clsx(
+        'max-w-[240px] cursor-pointer rounded-lg border bg-white p-4 shadow-xs',
+        className
+      )}
+      {...props}
+    >
+      <Image src={companyAvatar} alt="Avatar Nubank" />
       <Text fontStyle="mdm" className="mt-2 text-gray-700">
-        Nubank SA
+        {companyName}
       </Text>
       <Text fontStyle="smr" className="mb-4 text-gray-500">
-        Saúde Integral III
+        {projectName}
       </Text>
+      <div className="flex w-full gap-2">
+        <Tag text={tagsInfo.time} />
+        <Tag text={tagsInfo.rating} icon={tagsInfo.icon} />
+      </div>
     </div>
   )
 }
